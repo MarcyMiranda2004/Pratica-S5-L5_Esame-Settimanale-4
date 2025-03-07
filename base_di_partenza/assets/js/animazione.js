a64d62e5933db0f3e21d977627b1e7dd696837e8
Marcello Miranda <marcellomiranda2004@gmail.com>
2025-03-07 17:18:39 +0100

    text correction

--- base_di_partenza/assets/js/animazione.js
+++ base_di_partenza/assets/js/animazione.js
@@ -1,16 +1,1 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // Ottieni tutte le lettere "M"
-  const letters = document.querySelectorAll("#letters-container text");
 
-  // Funzione per nascondere/mostrare una lettera in modo casuale
-  function toggleLetterVisibility() {
-    const randomIndex = Math.floor(Math.random() * letters.length); // Scegli un indice casuale
-    const selectedLetter = letters[randomIndex];
-
-    // Cambia la classe per nascondere o mostrare la lettera
-    selectedLetter.classList.toggle("hidden");
-  }
-
-  // Esegui la funzione ogni 500ms
-  setInterval(toggleLetterVisibility, 500);
-});
