189d6f11ea58b70440d3c04833268540e9e5bc1b
Marcello Miranda <marcellomiranda2004@gmail.com>
2025-03-07 16:29:56 +0100

    navbar

--- base_di_partenza/assets/js/navbar.js
+++ base_di_partenza/assets/js/navbar.js
@@ -1,12 +1,12 @@
 window.onscroll = function () {
   let nav = document.querySelector("header nav");
-  let getStartedButton = document.querySelector("header nav .get-started");
+  let getStartedButton = document.querySelector("#buttonContainer .getStarted");
 
   if (window.scrollY > 0) {
     nav.classList.add("scrolled");
-    getStartedButton.classList.add("scrolled");
+    getStartedButton.style.backgroundColor = "#1a8917";
   } else {
     nav.classList.remove("scrolled");
-    getStartedButton.classList.remove("scrolled");
+    getStartedButton.style.backgroundColor = "#191919";
   }
 };
